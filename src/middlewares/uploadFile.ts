73e090ad8c5228deb950a5355ab98f4aef3a87e4
iamtonmoy0 <tonmoy2code@gmail.com>
2024-04-02 05:31:51 +0600

    "Update uploadFile.ts: add file filter and move filename to new line for readability"
    The diff includes changes to the storage function for handling file uploads, with the addition of a file filter function to check for allowed file types. Also, the filename function has been moved to a new line for improved readability.

--- src/middlewares/uploadFile.ts
+++ src/middlewares/uploadFile.ts
@@ -1,15 +1,31 @@
 import multer from "multer";
 import path from "path";
+
+// storage
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, process.env.UPLOAD_DIR);
   },
   filename: function (req, file, cb) {
     const exectName = path.extname(file.originalname);
-    cb(null, Date.now() + "-" + file.originalname.replace(exectName, "")+exectName);
+    cb(
+      null,
+      Date.now() + "-" + file.originalname.replace(exectName, "") + exectName
+    );
   },
 });
+// file filter system
+const fileFilter = (req, file, cb) => {
+  const exectName = path.extname(file.originalname);
+  const fileType = ["jpg", "png"];
+  if (!fileType.includes(exectName.substring(1))) {
+    return cb("file type is not allowed");
+  }
+  cb(null, true);
+};
 
-const upload = multer({ storage: storage });
+// there can be added file size
+const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 export default upload;
