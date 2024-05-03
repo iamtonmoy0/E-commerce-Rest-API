76424f18a333dcdf18bbe62f8ae0252ac04ca562
iamtonmoy0 <tonmoy2code@gmail.com>
2024-05-03 19:24:10 +0600

    "Add validation for email and password in registration middleware"
    This commit adds validation for the email and password fields in the registration middleware. The email field is required and cannot be empty, and the password field cannot be empty as well.

--- src/middlewares/auth.validation.ts
+++ src/middlewares/auth.validation.ts
@@ -9,4 +9,12 @@ export const registrationValidation = [
     .withMessage(
       "name should be minimum 3 characters long and maximum 30 characters"
     ),
+  body("email").trim().notEmpty().withMessage("Email  is required"),
+  body("password")
+    .notEmpty()
+    .withMessage("Password cant be empty")
+    .isLength({ min: 6, max: 20 })
+    .withMessage(
+      "Password should be minimum 6 characters long and maximum 20 characters"
+    ),
 ];
